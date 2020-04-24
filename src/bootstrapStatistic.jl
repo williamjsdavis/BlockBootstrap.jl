c4d44fc237dcd0e5521fa3e39b0920eca57159b7
william davis <williamjsdavis@berkeley.edu>
2020-04-24 13:12:13 -0700

    Tidied up syntax

--- src/bootstrapStatistic.jl
+++ src/bootstrapStatistic.jl
@@ -6,20 +6,20 @@ struct bootstrapResult
 end
 function bootstrapStatistic(data::Array{<:Real,1}, statisticHandle::Function, bootstrapSampleHandle::Function, blockLength::Integer, NbootstrapReplicates::Integer)
         # Bootstrap a statistic
-        fullDataEstimate = statisticHandle(data);
-        estimateType = typeof(fullDataEstimate);
+        fullDataEstimate = statisticHandle(data)
+        estimateType = typeof(fullDataEstimate)
 
         # Default number of blocks
-        Ndata = length(data);
-        Nblocks = ceil(Int, Ndata/blockLength);
+        Ndata = length(data)
+        Nblocks = ceil(Int, Ndata/blockLength)
 
         # Sampling
-        resampleIndexBB, resampleDataBB = bootstrapSampleHandle(data, blockLength, Nblocks, NbootstrapReplicates);
+        resampleIndexBB, resampleDataBB = bootstrapSampleHandle(data, blockLength, Nblocks, NbootstrapReplicates)
 
         # Statistic on samples
-        replicateEstimate = Vector{estimateType}(undef, NbootstrapReplicates);
+        replicateEstimate = Vector{estimateType}(undef, NbootstrapReplicates)
         for ii in 1:NbootstrapReplicates
-                replicateEstimate[ii] = statisticHandle(resampleDataBB[ii]);
+                replicateEstimate[ii] = statisticHandle(resampleDataBB[ii])
         end
 
         bootstrapResultOut = bootstrapResult(fullDataEstimate, replicateEstimate, resampleIndexBB, resampleDataBB)
